75d3acd2406249c65551d3f36eafb4f35252d605
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-09-16 19:25:20 -0300

    Agrega parte de la resolucion para ejercicio-7 En este caso los cambios corresponden a la conversion de decimal a hexadecimal.

--- codigo-mio/practica-1/ejercicio-7.c
+++ codigo-mio/practica-1/ejercicio-7.c
@@ -1,11 +1,12 @@
 #include "punteros.h"
 #include <math.h>
+#include <string.h>
 
 #define MAX_DIGITS 10
 
-/* 7) Escribir una funcion que reciba como argumento un entero y retorne una cadena de
-    caracteres en su representacion decimal. Idem para: representacion octal,
-    hexadecimal y binaria (genere una funcion por cada una de estas opciones).
+/* 7) Escribir una funci�n que reciba como argumento un entero y retorne una cadena de
+    caracteres en su representaci�n decimal. �dem para: representaci�n octal,
+    hexadecimal y binaria (genere una funci�n por cada una de estas opciones).
 */
 
 int * convertDecimalToOctal(int decimal, int *octal);
@@ -13,25 +14,26 @@ char * convertDecimalToHexa(int decimal, char *hexa);
 int * convertDecimalToBinary(int decimal);
 
 int resuelveDesafioSiete() {
-    int decimal = 1073741824;
+    int decimal = 99902303;
 
-    int *octalNum = (int *)malloc((MAX_DIGITS) * sizeof(int));
+    int *octalNum = (int *)malloc(MAX_DIGITS * sizeof(int));
     if(octalNum == NULL) {
         return EXIT_FAILURE;
     }
 
-    char *hexaNum = (char *)malloc(MAX_DIGITS);
+    char *hexaNum = (char *)calloc(MAX_DIGITS, sizeof(char));
     if(hexaNum == NULL) {
         return EXIT_FAILURE;
     }
 
-        //Octal
+    //1.    Octal
     printf("\tEnteros positivos en octal\n");
     printf("Cada uno se representara a traves de 40bytes, por ello\n");
     printf("el maximo numero a representar sera de 10 digitos.\n");
     printf("Es decir, 1073.741.823 numeros en decimal.\n");
     convertDecimalToOctal(decimal, octalNum);
 
+    printf("\nEl numero corresponde en octal a: ");
     for(int i = 0; i <= 9; i++){
         printf("%d", octalNum[i]);
     }
@@ -41,9 +43,14 @@ int resuelveDesafioSiete() {
      ya que le faltaran digitos a la representacion.
     */
 
-        //Hexa
-    printf("\tNumeros en hexadecimal");
+    //2.    Hexa
+    printf("\n\tEnteros positivos en hexadecimal");
     convertDecimalToHexa(decimal, hexaNum);
+
+    printf("\nEl numero corresponde en hexadecimal a: ");
+    printf("%s\n", hexaNum);
+
+    printf("\nFin del programa.\n");
 
     free(octalNum);
     free(hexaNum);
@@ -52,18 +59,15 @@ int resuelveDesafioSiete() {
 }
 
 int * convertDecimalToOctal(int decimal, int *octalNum) {
-    int remains[MAX_DIGITS];
-    int index = 0;
+    int remains[MAX_DIGITS], base = 8, index = 0;
 
     while(decimal != 0){
-        remains[index] = decimal % 8;
-        decimal = floor(decimal / 8);
+        remains[index] = decimal % base;
+        decimal = getQuotient(decimal, base);
         index++;
     }
 
-    printf("\nIndice vale: %d", index);
     int numDigits = MAX_DIGITS - (index);
-    printf("\nDigitos: %d", numDigits);
 
     for(int i = 0; i < MAX_DIGITS; i++){
         if(i >= numDigits) {
@@ -79,7 +83,102 @@ int * convertDecimalToOctal(int decimal, int *octalNum) {
     return octalNum;
 }
 
-char * convertDecimalToHexa(int decimal, char *hexaNum) {
+char * convertDecimalToHexa(int decimalNum, char *hexaNum) {
+    int dividend, quotient, decDigit, index = 0, base = 16;
+
+    dividend = decimalNum;
+
+    while(quotient >= base){
+        quotient = getQuotient(dividend, base);
+        decDigit = dividend - (quotient * base);
+        int hexDigit = getHexDigit(decDigit);
+        hexaNum[index] = hexDigit;
+        index++;
+
+        if(quotient < base){
+        //ultimo digito que pasar a base 16
+            decDigit = quotient;
+            hexDigit = getHexDigit(decDigit);
+            hexaNum[index] = hexDigit;
+        } else {
+            dividend = quotient;
+        }
+    }
+
+    //Ordenar el numero mediante el puntero
+    int length = index;
+    int a, b = length;
+
+    for(a = 0; a <= b ; a++){
+        char aux = hexaNum[a];
+        hexaNum[a] = hexaNum[b];
+        hexaNum[b] = aux;
+        b--;
+    }
 
     return hexaNum;
+}
+
+int getQuotient(int dividend, int divisor) {
+    int quotient = floor(dividend / divisor);
+
+    return quotient;
+}
+
+char getHexDigit(int decDigit) {
+    char hexDigit;
+
+    switch(decDigit){
+        case 0:
+            hexDigit = '0';
+            break;
+        case 1:
+            hexDigit = '1';
+            break;
+        case 2:
+            hexDigit = '2';
+            break;
+        case 3:
+            hexDigit = '3';
+            break;
+        case 4:
+            hexDigit = '4';
+            break;
+        case 5:
+            hexDigit = '5';
+            break;
+        case 6:
+            hexDigit = '6';
+            break;
+        case 7:
+            hexDigit = '7';
+            break;
+        case 8:
+            hexDigit = '8';
+            break;
+        case 9:
+            hexDigit = '9';
+            break;
+        case 10:
+            hexDigit = 'A';
+            break;
+        case 11:
+            hexDigit = 'B';
+            break;
+        case 12:
+            hexDigit = 'C';
+            break;
+        case 13:
+            hexDigit = 'D';
+            break;
+        case 14:
+            hexDigit = 'E';
+            break;
+        case 15:
+            hexDigit = 'F';
+            break;
+        default:
+            printf("\nUps! Parece que hubo un error.\n");
+    }
+    return hexDigit;
 }
